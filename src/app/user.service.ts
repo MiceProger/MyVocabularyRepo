72821707721ef3fdd31fdc6a6dadf5e0577fabae
MiceProger <sabovmicha@gmail.com>
2022-06-09 16:23:07 +0300

    fix

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { userInfo } from 'os';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
