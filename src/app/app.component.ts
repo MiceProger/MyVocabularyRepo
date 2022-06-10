85cb7f500125e1dcbf5e5982a3d806a3bb19d838
MiceProger <sabovmicha@gmail.com>
2022-06-10 16:05:57 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { HomeComponent } from "./homePage/home/home.component";
 
 
 @Component({
